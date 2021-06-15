8b64bd793d3fe286f9ed03c90037fab22eb60e80
raityupiyo <42093052+piyoraik@users.noreply.github.com>
2021-06-15 14:55:53 +0900

    choice

--- lib/step-functions-stack.ts
+++ lib/step-functions-stack.ts
@@ -12,14 +12,14 @@ export class StepFunctionsStack extends Stack {
 	constructor(scope: Construct, id: string, props?: StackProps) {
 		super(scope, id, props);
 
-		// Lambda関数の作成
+		//! Lambda関数の作成
 		const taskFn = new lambda.Function(this, "TaskFn", {
 			runtime: lambda.Runtime.NODEJS_12_X,
 			code: lambda.Code.fromAsset("handlers"),
 			handler: "task.handler",
 		});
 
-		// StepFunctionsのタスク定義
+		//! StepFunctionsのタスク定義
 		const firstState = new tasks.LambdaInvoke(this, "最初の処理", {
 			lambdaFunction: taskFn,
 			// Taskのoutputを指定、$.Payloadを指定することでLambdaの戻り値を次のTaskへ渡す
@@ -43,14 +43,35 @@ export class StepFunctionsStack extends Stack {
 			lambdaFunction: taskFn,
 		});
 
+		//! 並列処理(Parallel)の実装
 		// Parallelクラスのインスタンスを作成
-		const parallel = new sfn.Parallel(this, "Parallel");
+		// const parallel = new sfn.Parallel(this, "Parallel");
 		// ブランチ関数を使い並列処理stateを指定する
-		parallel.branch(paralellState1, paralellState2).next(wait10);
+		// parallel.branch(paralellState1, paralellState2).next(wait10);
 
-		// ステートマシンを作成するクラスの引数に渡す
+		//! 条件分岐(Choice)の実装
+		// Choiceクラスのインスタンスを作成
+		const choice = new sfn.Choice(this, "ChoiceState");
+		//when()メソッドで条件分岐を定義
+		choice.when(
+			// 前のstateから渡されたoutputのmessageの値がHelloなら処理1に遷移
+			sfn.Condition.stringEquals("$.message", "Hello"),
+			new sfn.Pass(this, "処理1")
+		);
+		choice.when(
+			sfn.Condition.stringEquals("$.message", "World"),
+			new sfn.Pass(this, "処理2")
+		);
+		// whenが一致しなかった場合
+		choice.otherwise(new sfn.Pass(this, "処理3"));
+
+		//! 処理順番の定義
+		// Taskを.next()でつなぐ、戻り値としてステートマシンの実態が返却
+		const definition = firstState.next(choice);
+
+		//! ステートマシンを作成するクラスの引数に渡す
 		new sfn.StateMachine(this, "stateMachine", {
-			definition: parallel,
+			definition,
 		});
 	}
 }
