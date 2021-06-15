386bb706475a0b11c60f6bc15c13e32cd0b6bfa0
raityupiyo <42093052+piyoraik@users.noreply.github.com>
2021-06-15 14:10:49 +0900

    ic

--- lib/step-functions-stack.ts
+++ lib/step-functions-stack.ts
@@ -1,10 +1,41 @@
-import { Stack, StackProps } from 'aws-cdk-lib';
-import { Construct } from 'constructs';
+import {
+	Stack,
+	StackProps,
+	aws_lambda as lambda,
+	aws_stepfunctions as sfn,
+	aws_stepfunctions_tasks as tasks,
+} from "aws-cdk-lib";
+import { Construct } from "constructs";
 
 export class StepFunctionsStack extends Stack {
-  constructor(scope: Construct, id: string, props?: StackProps) {
-    super(scope, id, props);
+	constructor(scope: Construct, id: string, props?: StackProps) {
+		super(scope, id, props);
 
-    // The code that defines your stack goes here
-  }
+		// Lambda関数の作成
+		const taskFn = new lambda.Function(this, "TaskFn", {
+			runtime: lambda.Runtime.NODEJS_12_X,
+			code: lambda.Code.fromAsset("handlers"),
+			handler: "task.handler",
+		});
+
+		// StepFunctionsのタスク定義
+		const firstState = new tasks.LambdaInvoke(this, "最初の処理", {
+			lambdaFunction: taskFn,
+			// Taskのoutputを指定、$.Payloadを指定することでLambdaの戻り値を次のTaskへ渡す
+			outputPath: "$.Payload",
+		});
+		const secondState = new tasks.LambdaInvoke(this, "次の処理", {
+			// Taskのinputを指定、$とすることで、上のTaskのJSONがすべてInputに渡る
+			inputPath: "$",
+			lambdaFunction: taskFn,
+		});
+
+		// Taskを.next()でつなぐ、戻り値としてステートマシンの実態が返却
+		const definition = firstState.next(secondState);
+
+		// ステートマシンを作成するクラスの引数に渡す
+		new sfn.StateMachine(this, "stateMachine", {
+			definition: definition,
+		});
+	}
 }
